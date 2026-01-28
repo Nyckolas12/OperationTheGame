6da687acf3337fcc2b846a0e98b95af0e25f44ae
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2026-01-27 22:59:36 -0700

    Very Basic tweezers

--- Operation/Assets/Objects/Tweezers/TweezersMovement.cs
+++ Operation/Assets/Objects/Tweezers/TweezersMovement.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TweezersMovement : MonoBehaviour
 {
+    //Jadan: we should probably shange these to be assigned to a ScriptableObject later so that its easier to tweak
     [Header("Movement")]
     public float moveSpeed = 0.15f;
     public float verticalSpeed = 0.1f;
@@ -9,10 +12,31 @@ public class TweezersMovement : MonoBehaviour
     [Header("Rotation")]
     public float rotateSpeed = 80f;
 
+    [Header("Clamp Speed")]
+    public float clampSpeed = 80f;
+    
+
+    public float testvar = 1f;
+
+    //
+    private GameObject leftProng;
+    private GameObject rightProng;
+    
+
+    private void Start()
+    {
+        List<Transform> allDescendants = this.GetComponentsInChildren<Transform>(true).ToList();
+        //:(
+        leftProng = allDescendants.Where(a => a.gameObject.name == "leftP").First().gameObject;
+        rightProng = allDescendants.Where(a => a.gameObject.name == "rightP").First().gameObject;
+
+    }
+
     private void Update()
     {
         HandleMovement();
         HandleRotation();
+        HandleClamping();
     }
 
     void HandleMovement()
@@ -41,5 +65,27 @@ public class TweezersMovement : MonoBehaviour
 
         transform.Rotate(Vector3.up, rotX, Space.World);
         transform.Rotate(Vector3.right, -rotY, Space.Self);
+    }
+
+    //animations for closing
+    //if close button == pressed
+    //  if left rotation >= 90 and right rotation >=-90 then do nothing,
+    //  else 
+    //      increase rotations by a certain amount each update * dt
+    //else
+    //  return to default position
+
+    void HandleClamping()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            leftProng.transform.localRotation = Quaternion.Euler(0, 90, 90);
+            rightProng.transform.localRotation = Quaternion.Euler(0, -90, -90);
+        }
+        else
+        {
+            leftProng.transform.localRotation = Quaternion.Euler(0, 70, 90);
+            rightProng.transform.localRotation = Quaternion.Euler(0, -70, -90);
+        }
     }
 }
